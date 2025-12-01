4256d975550c2b41226a0e3a5a85cb6023aff1cd
abrshz <trusewynur@gmail.com>
2025-12-01 12:41:48 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,128 @@
+
+//1 
+
+// Single comments:- Welcome to JavaScript Class 
+
+// Multiple comments:-  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+
+// 2
+
+let studentName = "Johnson";
+
+console.log(studentName);
+
+let isMenuActive = false;
+
+console.log(isMenuActive);
+
+
+// 3
+
+const speedOfLight = 299792458;
+
+console.log(speedOfLight);
+
+
+// 4
+
+console.log(typeof studentName);
+
+console.log(typeof isMenuActive);
+
+console.log(typeof speedOfLight);
+
+
+// 5  
+
+let question = "What is your name"
+
+console.log(question);
+
+
+// 6 
+
+let x = 5;
+
+let y = 8; 
+
+console.log(x + y);
+
+console.log(y - x);
+
+console.log(y % x);
+
+// 7
+
+let age = 18;
+
+const isAdult = age >= 18;
+
+console.log(isAdult);
+
+// 8
+
+// Undefined:- not assigned the variable existing but currently lacks a definition.
+
+// Null:- intentional absence of an object value it means empty or no value.
+
+let quantity;
+
+console.log(quantity);
+
+console.log(typeof quantity);
+
+let result = quantity + 10;
+
+console.log(result);
+
+// 9 
+
+let country = "Uganda";
+
+console.log(country);
+
+console.log(typeof country);
+
+
+let total = 4500;
+
+console.log(total);
+
+console.log(typeof total);
+
+
+let hasPaid = false;
+
+console.log(hasPaid);
+
+console.log(typeof hasPaid);
+
+
+let data;
+
+console.log(data);
+
+console.log(typeof data);
+
+let amountPaid = data + 100;
+
+console.log(amountPaid);
+
+// 10
+
+let counter = 5;
+
+counter = counter * 3;
+counter = counter - 2;
+
+console.log(counter);
+
+
+
+
+
+
+
+
+
