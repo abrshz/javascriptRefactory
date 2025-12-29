11bfafec13af6f6f0227459cafeb7a34fad7a0ec
abrshz <trusewynur@gmail.com>
2025-12-29 22:38:25 +0300

    Done part A from the assighnment

---
+++ KGL Data Validation System/kgl_validation.js
@@ -0,0 +1,29 @@
+
+// 1)
+let companyName = 'Karibu Groceries LTD';
+let minimumTonnage = 1000;
+let isOperational = true;
+let managerName;
+let closeBranches = null;
+
+// 2)
+console.log(companyName);  //String
+console.log(minimumTonnage); //Number
+console.log(isOperational); //Boolean
+console.log(managerName); //Undefine
+console.log(closeBranches); //Null
+
+// 3)
+
+
+// Part B: String Manipulation and Validation 
+
+// 4) 
+let dealerNameInput = " james BOND ";
+
+// 5)
+console.log(dealerNameInput.trim());
+
+
+
+
